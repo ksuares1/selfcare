e3d6bf753085bd58c59026745e92ec1b0b76c68d
ksuares1 <kimberlyjsuares@gmail.com>
2022-05-16 14:55:21 -0400

    added carousel

---
+++ selfcare/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home';
+function App() {
+  return (
+    <div>
+     <>
+     <Router>
+       <Routes>
+         <Route  path="/" element={<Home/>} />
+         </Routes>
+     </Router>
+     </>
+    </div>
+  );
+}
+
+export default App;
---
+++ selfcare/src/Pages/Home.js
@@ -0,0 +1,13 @@
+import Homepage from "../components/Homepage";
+
+const Home=() =>{
+    return(
+        <main>
+            <div>
+                <Homepage/>
+            </div>
+        </main>
+    )
+}
+
+export default Home;
