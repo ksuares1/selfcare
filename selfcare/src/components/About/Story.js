3ae270baad984caa48a13c057884a64548a5e92b
ksuares1 <kimberlyjsuares@gmail.com>
2022-05-16 23:02:05 -0400

    added google font and text to about section

--- selfcare/src/components/About/Story.js
+++ selfcare/src/components/About/Story.js
@@ -1,14 +1,15 @@
 
 
-
 const Brand=()=>{
     return(
-    <div>
-        <h1>
+    <div class="brand-story">
+        <h1 class="info">
             Welcome to Tropical Oasis.
-            <h2>Let the soothing sounds of ocean waves renew and renergize you, as you sip your favorite tropical drinks, while receivig a pampering foot massage.  </h2>
-        </h1>
-        
+            </h1>
+            {/* <h2>Let the soothing sounds of ocean waves renew and renergize you, as you sip your favorite tropical drinks, while receivig a pampering massage or facial.  </h2> */}
+            <p class="about-para">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia diam id mi pharetra, at malesuada diam faucibus. Duis at elit vitae sem semper egestas eleifend ut enim. Donec mattis dapibus ex, quis gravida ex consectetur sed. Vivamus scelerisque venenatis elementum. In dignissim auctor bibendum. Integer vestibulum est id metus pellentesque, non malesuada tellus aliquam. Nullam a aliquam est. Curabitur sed tempor tortor, mollis volutpat sapien. Donec et quam eleifend nisi vestibulum faucibus.
+            </p>
 </div>
     )
 }
